a0580e697815108a299fc703da9eefec29a9e4bd
Rayhan-Shhadeh <re7an.ma7moud@gmail.com>
2023-10-01 04:10:09 -0700

    Initial commit

---
+++ index.php
@@ -0,0 +1,162 @@
+    <!doctype html>
+    <html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport"
+              content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+        <meta http-equiv="X-UA-Compatible" content="ie=edge">
+        <title>Let's Play</title>
+
+
+
+
+        <!-- Fonts -->
+        <link rel="preconnect" href="https://fonts.googleapis.com">
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+        <link href="https://fonts.googleapis.com/css2?family=Heebo:wght@400;500;600&family=Inter:wght@700;800&display=swap" rel="stylesheet">
+
+        <!-- Icon Font Stylesheet -->
+        <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.10.0/css/all.min.css" rel="stylesheet">
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.4.1/font/bootstrap-icons.css" rel="stylesheet">
+
+        <!-- Libraries Stylesheet -->
+        <link href="lib/animate/animate.min.css" rel="stylesheet">
+        <link href="lib/owlcarousel/assets/owl.carousel.min.css" rel="stylesheet">
+
+        <!-- Customized Bootstrap Stylesheet -->
+        <link href="css/bootstrap.min.css" rel="stylesheet">
+
+        <!-- CSS Stylesheet -->
+        <link href="css/style.css" rel="stylesheet">
+        <!-- Required meta tags -->
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+        <title>gaming</title>
+        <link rel="icon" href="img/favicon.png">
+        <!-- Bootstrap CSS -->
+        <link rel="stylesheet" href="css/bootstrap.min.css">
+        <!-- animate CSS -->
+        <link rel="stylesheet" href="css/animate.css">
+        <!-- owl carousel CSS -->
+        <link rel="stylesheet" href="css/owl.carousel.min.css">
+        <!-- font awesome CSS -->
+        <link rel="stylesheet" href="css/all.css">
+        <!-- flaticon CSS -->
+        <link rel="stylesheet" href="css/flaticon.css">
+        <link rel="stylesheet" href="css/themify-icons.css">
+        <!-- font awesome CSS -->
+        <link rel="stylesheet" href="css/magnific-popup.css">
+        <!-- swiper CSS -->
+        <link rel="stylesheet" href="css/slick.css">
+        <!-- style CSS -->
+        <link rel="stylesheet" href="css/style.css">
+
+        <!-- Icon Font Stylesheet -->
+        <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.10.0/css/all.min.css" rel="stylesheet">
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.4.1/font/bootstrap-icons.css" rel="stylesheet">
+
+        <!-- Libraries Stylesheet -->
+        <link href="lib/animate/animate.min.css" rel="stylesheet">
+        <link href="lib/owlcarousel/assets/owl.carousel.min.css" rel="stylesheet">
+
+        <script src="https://cdn.lordicon.com/bhenfmcm.js"></script>
+        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
+    </head>
+
+    <body class="body_bg">
+    <!--::header part start::-->
+<?php include('header.php'); ?>
+    <div class="bg-video">
+        <video autoplay loop muted>
+            <source src="/img/bg.mp4" type="video/mp4">
+            Your browser does not support the video tag.
+        </video>
+    </div>
+    <div class="lets-play">
+    <h2>LET'S PLAY</h2>
+    </div>
+
+
+
+    <!-- Dorm Start -->
+    <div class="container-xxl py-5">
+        <div class="container align-content-center align-items-center">
+            <div class="text-center mx-auto mb-5 wow fadeInUp" data-wow-delay="0.1s" style="max-width: 700px;">
+                <h1 class="mb-3">What's your Favourite? </h1>
+            </div>
+            <div class="row g-3 justify-content-center">
+
+
+                <div class="col-lg-3 col-md-6 wow fadeInDown" data-wow-delay="0.5s">
+                    <div class="team-item rounded overflow-hidden">
+                        <div class="position-relative">
+                            <div style="background-color: #fff">
+                                <lord-icon
+                                        src="https://cdn.lordicon.com/ndfdylqe.json"
+                                        trigger="morph"
+                                        colors="primary:#a5ebff,secondary:#000000"
+                                        b
+                                        style="width:250px;height:265px">
+                                </lord-icon>
+                            </div>
+                            <div class="position-absolute start-50 top-100 translate-middle d-flex align-items-center">
+                            </div>
+                        </div>
+                        <div class="text-center p-4 mt-3">
+                            <h5 class="fw-bold mb-0"> Puzzles </h5>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-lg-3 col-md-6 wow fadeInUp" data-wow-delay="0.5s">
+                    <div class="team-item rounded overflow-hidden">
+                        <div class="position-relative">
+                            <a class=" mx-1" href="puzzles.php"><img class="img-fluid" src="img/chess.gif" alt=""></i></a>
+                            <div class="position-absolute start-50 top-100 translate-middle d-flex align-items-center">
+
+                            </div>
+                        </div>
+                        <div class="text-center p-4 mt-3">
+                            <h5 class="fw-bold mb-0"> Chess </h5>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-lg-3 col-md-6 wow fadeInUp" data-wow-delay="0.5s">
+                    <div class="team-item rounded overflow-hidden">
+                        <div class="position-relative">
+                            <a class=" mx-1" href="puzzles.php"><img class="img-fluid" src="img/brain.gif" alt=""></i></a>
+                            <div class="position-absolute start-50 top-100 translate-middle d-flex align-items-center">
+
+                            </div>
+                        </div>
+                        <div class="text-center p-4 mt-3">
+                            <h5 class="fw-bold mb-0"> Brain Games </h5>
+                        </div>
+                    </div>
+                </div>
+
+
+            </div>
+        </div>
+    </div>
+    <!-- Dorm End -->
+
+
+    <section class="">
+        <div class="">
+            <div class="row align-items-center justify-content-between" style="padding-left: 10px">
+                <div class="col-lg-6 col-md-8">
+                    <div class="banner_text">
+                        <div class="banner_text_iner">
+                            <h1>Best Highlights
+                                of the Latest Puzzles</h1>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <!-- Template Javascript -->
+
+    </body>
+    </html>
