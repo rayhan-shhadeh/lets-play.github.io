879ccbc1987dae86e688ebdc64a6956d00a9eebd
RayhanShadeh <51097934+rayhan-shhadeh@users.noreply.github.com>
2024-01-12 12:52:27 +0200

    Update index.php header
    header

--- index.php
+++ index.php
@@ -65,8 +65,61 @@
 
     <body class="body_bg">
     <!--::header part start::-->
-<?php include('header.php'); ?>
-    <div class="bg-video">
+
+
+<header class="main_menu single_page_menu">
+    <div class="container">
+        <div class="row align-items-center">
+            <div class="col-lg-12">
+                <nav class="navbar navbar-expand-lg navbar-light slideInDown slideInLeft duration-3s">
+                    <lord-icon
+                        src="https://cdn.lordicon.com/ndfdylqe.json"
+                        trigger="morph"
+                        colors="primary:#4be1ec,secondary:#1663c7"
+                        style="width:100px;height:100px">
+                    </lord-icon>
+                    <button class="navbar-toggler" type="button" data-toggle="collapse"
+                            data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent"
+                            aria-expanded="false" aria-label="Toggle navigation">
+                        <span class="menu_icon"><i class="fas fa-bars"></i></span>
+                    </button>
+
+                    <div class="collapse navbar-collapse main-menu-item"   id="navbarSupportedContent">
+                        <ul class="navbar-nav">
+                            <li class="nav-item">
+                                <a class="nav-link" href="index.php">Home</a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link" href="cart.php">Cart</a>
+                            </li>
+
+
+                            <li class="nav-item dropdown">
+                                <a class="nav-link dropdown-toggle" href="c1.php" id="navbarDropdown1"
+                                   role="button" data-toggle="dropdown" aria-haspopup="true"
+                                   aria-expanded="false">
+                                    Categories
+                                </a>
+                                <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+                                    <a class="dropdown-item" href="chess.php"> Chess</a>
+                                    <a class="dropdown-item" href="puzzles.php">Puzzles</a>
+                                    <a class="dropdown-item" href="brain-games.php">Brain Games</a>
+                                </div>
+
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link" href="contact.php">Contact</a>
+                            </li>
+                        </ul>
+                    </div>
+
+                </nav>
+            </div>
+        </div>
+    </div>
+</header>
+<!-- Header part end-->
+        <div class="bg-video">
         <video autoplay loop muted>
             <source src="/img/bg.mp4" type="video/mp4">
             Your browser does not support the video tag.
